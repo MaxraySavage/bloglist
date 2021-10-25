9578802dfb97f409ed09d6b0b590e2e96f6414e1
MaxraySavage <maxraysavage@gmail.com>
2021-10-25 13:16:18 -0400

    use testHelper to contain test lists

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,68 +1,8 @@
 const listHelper = require('../utils/list_helper')
+const helper = require('./test_helper')
+
 
 const emptyList = []
-    
-const listWithOneBlog = [
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    }
-]
-
-const listWitMultipleBlogs = [
-    {
-        _id: '5a422a851b54a676234d17f7',
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: '5a422b3a1b54a676234d17f9',
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: '5a422ba71b54a676234d17fb',
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2,
-        __v: 0
-    }  
-]
 
 describe('total likes', () =>{
 
@@ -74,13 +14,13 @@ describe('total likes', () =>{
 
     test('singleton list returns likes value of sole element', () => {
 
-        const result = listHelper.totalLikes(listWithOneBlog)
+        const result = listHelper.totalLikes(helper.oneBlogList)
         expect(result).toBe(5)
     })
 
     test('list with multiple elements returns correct value', () => {
 
-        const result = listHelper.totalLikes(listWitMultipleBlogs)
+        const result = listHelper.totalLikes(helper.multipleBlogList)
         expect(result).toBe(36)
     })
 })
@@ -95,8 +35,8 @@ describe('favorite blog', () =>{
 
     test('singleton list returns sole element', () => {
 
-        const result = listHelper.favoriteBlog(listWithOneBlog)
-        expect(result).toBe(listWithOneBlog[0])
+        const result = listHelper.favoriteBlog(helper.oneBlogList)
+        expect(result).toBe(helper.oneBlogList[0])
     })
 
     test('list with multiple elements returns correct blog', () => {
@@ -108,7 +48,7 @@ describe('favorite blog', () =>{
             likes: 12,
             __v: 0
         }
-        const result = listHelper.favoriteBlog(listWitMultipleBlogs)
+        const result = listHelper.favoriteBlog(helper.multipleBlogList)
         expect(result).toEqual(expectedFavoriteBlog)
     })
 })
@@ -126,7 +66,7 @@ describe('author with most blogs', () =>{
             author: 'Edsger W. Dijkstra',
             blogs: 1
         }
-        const result = listHelper.mostBlogs(listWithOneBlog)
+        const result = listHelper.mostBlogs(helper.oneBlogList)
         expect(result).toEqual(expectedTopAuthor)
     })
 
@@ -135,7 +75,7 @@ describe('author with most blogs', () =>{
             author: 'Robert C. Martin',
             blogs: 3
         }
-        const result = listHelper.mostBlogs(listWitMultipleBlogs)
+        const result = listHelper.mostBlogs(helper.multipleBlogList)
         expect(result).toEqual(expectedTopAuthor)
     })
 })
@@ -154,7 +94,7 @@ describe('author with most likes', () =>{
             author: 'Edsger W. Dijkstra',
             likes: 5
         }
-        const result = listHelper.mostLikes(listWithOneBlog)
+        const result = listHelper.mostLikes(helper.oneBlogList)
         expect(result).toEqual(expectedTopAuthor)
     })
 
@@ -163,7 +103,7 @@ describe('author with most likes', () =>{
             author: 'Edsger W. Dijkstra',
             likes: 17
         }
-        const result = listHelper.mostLikes(listWitMultipleBlogs)
+        const result = listHelper.mostLikes(helper.multipleBlogList)
         expect(result).toEqual(expectedTopAuthor)
     })
 })
