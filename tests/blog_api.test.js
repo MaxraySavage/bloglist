3b3be5a83d58b969242cb45364ba83d35cd72c2a
MaxraySavage <maxraysavage@gmail.com>
2021-11-03 14:57:27 -0400

    update tests to work with auth

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
@@ -5,36 +7,61 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+let authHeader = 'bearer '
 
 beforeEach(async () => {
     await Blog.deleteMany({})
+    await User.deleteMany({})
 
-    for(const blog of helper.multipleBlogList) {
+    const passwordHash = await bcrypt.hash('sekret', 12)
+    const user = new User({
+        username: 'root',
+        name: 'Rudy',
+        passwordHash,
+        notes: []
+    })
+
+    const savedUser = await user.save()
+
+
+    for(let blog of helper.multipleBlogList) {
+        blog.user = savedUser.id
         const blogObject = new Blog(blog)
         await blogObject.save()
     }
+
+    const userForToken = {
+        username: savedUser.username,
+        id: savedUser.id,
+    }
+
+    const token = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET)
+
+    authHeader = 'bearer ' + token 
 })
 
 describe('when there are some blogs in the database', () => {
 
     test('blogs are returned as json', async () => {
-        await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+        await api.get('/api/blogs').set('Authorization', authHeader).expect(200).expect('Content-Type', /application\/json/)
     })
 
     test('all blogs are returned', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', authHeader)
 
         expect(response.body).toHaveLength(helper.multipleBlogList.length)
     })
 
     test('a specific blog is within the returned blogs', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', authHeader)
 
         expect(response.body[0].likes).toEqual(helper.multipleBlogList[0].likes)
     })    
 
     test('blogs are returned with id property not _id', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', authHeader)
 
         expect(response.body[0].id).toBeDefined()
 
@@ -54,6 +81,7 @@ describe('addition of a new blog', () => {
 
         await api
             .post('/api/blogs')
+            .set('Authorization', authHeader)
             .send(newBlog)
             .expect(201)
             .expect('Content-Type', /application\/json/)
@@ -74,6 +102,7 @@ describe('addition of a new blog', () => {
 
         await api
             .post('/api/blogs')
+            .set('Authorization', authHeader)
             .send(newBlog)
             .expect(201)
             .expect('Content-Type', /application\/json/)
@@ -98,11 +127,13 @@ describe('addition of a new blog', () => {
 
         await api
             .post('/api/blogs')
+            .set('Authorization', authHeader)
             .send(missingTitle)
             .expect(400)
 
         await api
             .post('/api/blogs')
+            .set('Authorization', authHeader)
             .send(missingUrl)
             .expect(400)
 
@@ -118,6 +149,7 @@ describe('deletion of a blog', () => {
     
         await api
             .delete(`/api/blogs/${blogToDelete.id}`)
+            .set('Authorization', authHeader)
             .expect(204)
     
         const blogsAtEnd = await helper.blogsInDb()
@@ -140,6 +172,7 @@ describe('updating a blog', () => {
 
         await api
             .put(`/api/blogs/${blogToChange.id}`)
+            .set('Authorization', authHeader)
             .send(blogToChange)
             .expect(200)
 
